17b0dba70330258cede31558ff2c796949619757
John Pham <mr.john.pham@gmail.com>
2020-04-05 06:55:46 -0400

    Propose potential solution to no repeats algorithm

---
+++ noRepeats.js
@@ -0,0 +1,26 @@
+function permAlone(str) {
+    return str;
+  }
+  
+  permAlone('aab');
+  
+
+
+/*** Thoughts on Tackling This Problem
+ * Break up the string into letters
+ * Store each letter as a key in an object
+ * 
+ * I need a helper function that generates permutations and stores each permutation in an array
+ * 
+ * Create an array that stores permutations that don't have repeating characters
+ * 
+ * Iterate through the permutation array
+ *      Create a variable that stores the most recently seen character
+ *      Iterate through each array string
+ *          If current character equals the most recently seen character, skip to next array element
+ *              Otherwise, set most recently seen character to current character and continue
+ *          If I iterate successfully through an array string and no repeats were found, save the string in the array that stores non-repeating permutations
+ *
+ * Return the length of the array that stores non-repeating permutations
+ *  
+*/
